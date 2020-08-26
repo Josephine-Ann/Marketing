086b0fe2d4bec5911366312c2722eabf1c52e7a1
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-08-26 12:52:19 +0200

    router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const LandingPage = () => (
+    <div>
+        This is from my landing page component
+    </div>
+);
+
+const AboutPage = () => (
+    <div>
+        This is from my about page component
+    </div>
+);
+
+const ProductPage = () => (
+    <div>
+        This is from my product page component
+    </div>
+);
+
+const ContactPage = () => (
+    <div>
+        This is from my contact page component
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        This is from my 404 page - <Link to="/">Go home</Link>
+    </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Marketing App</h1>
+    </header>
+)
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={LandingPage} exact={true} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/product" component={ProductPage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
 
