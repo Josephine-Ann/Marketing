95f0c677f5595abafef90e02f4222734362b8bee
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-08-26 21:49:51 +0200

    change title

--- src/components/LandingImageTitle.js
+++ src/components/LandingImageTitle.js
@@ -4,8 +4,8 @@ const LandingImageTitle = () => (
     <section id="banner">
         <div className="inner">
             <div className="logo"><span className="icon fa-gem"></span></div>
-            <h2>This is Solid State</h2>
-            <p>Another free + fully responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
+            <h2>Marketing Website</h2>
+            <p>Learn all about our new products!</p>
         </div>
     </section>
 );
